e78a3280178e196e882d7ae1d22f8275d500fd67
Roxanna Andrade <roxanna.andrade@yale.edu>
2020-03-17 19:22:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ age = 19
 favorite_food = "cheese"
 height_in_inches = 65
 above =  height_in_inches > 64
-height_message = above ? "You are taller than average."
+height_message = above ? "You are taller than average." : "You are not taller than average."
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
