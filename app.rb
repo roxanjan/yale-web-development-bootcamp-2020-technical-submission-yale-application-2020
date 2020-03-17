1e6e658abf9e88278233fe7516d25d1fc4402762
Roxanna Andrade <roxanna.andrade@yale.edu>
2020-03-17 19:20:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,7 +2,11 @@ require_relative './test'
 AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
-
+name = "Roxanna"
+age = 19
+favorite_food = "cheese"
+height_in_inches = 65
+height_message = 
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
